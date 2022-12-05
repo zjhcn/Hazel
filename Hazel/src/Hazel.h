f4ce6724a0c09ff309d9568407d0267ca044bced
hank <hank-c@qq.com>
2022-12-06 01:13:06 +0800

    feat: add log

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // For use by Hazel applictions
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---Entry Point-------------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,10 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("Initialized Log!");
+	HZ_INFO("Hello!");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
